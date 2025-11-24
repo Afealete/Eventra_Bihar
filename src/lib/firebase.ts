34dc02fac62fa2e2a718a94e8739da2c06d483a5
Afealete <doeemmanuel948@gmail.com>
2025-11-24 09:52:24 -0800

    firebase configured

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAxP4pl6q6Zl84-KXAD-ikj9oqEAeUWcfo",
+  authDomain: "eventra-ec344.firebaseapp.com",
+  projectId: "eventra-ec344",
+  storageBucket: "eventra-ec344.firebasestorage.app",
+  messagingSenderId: "4291094550",
+  appId: "1:4291094550:web:aec407bc22993948362d37",
+  measurementId: "G-JYYF5TC1SE"
 };
 
 const app = initializeApp(firebaseConfig);
